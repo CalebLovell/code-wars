1a5bc73cb9bb5b3214733f50d1e113e86610a5e3
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-06-11 15:26:14 -0600

    Create code-wars-4.js

---
+++ code-wars-4.js
@@ -0,0 +1,8 @@
+function findNextSquare(sq) {
+  const squareRoot = Math.sqrt(sq);
+  if (Number.isInteger(squareRoot)) {
+    return Math.pow(squareRoot + 1, 2);
+  } else {
+    return -1;
+  }
+}
